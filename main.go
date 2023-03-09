d81da621b950fa2d67c94c2dbc0192ec1bdd1701
Caleb Hicks <calebhicks1@gmail.com>
2023-03-09 13:37:08 -0500

    Added ability to create gravity source

--- main.go
+++ main.go
@@ -12,16 +12,16 @@ import (
 	"golang.org/x/image/colornames"
 )
 
-const AtomWidth = 8
+const AtomWidth = 2
 const AtomMass = 1
-const Gravity = -3000
+const Gravity = 1000
 const Friction = 0.5
 
 var (
 	frames      = 0
 	second      = time.Tick(time.Second)
 	color       = pixel.RGB(1, 0, 0)
-	WindowColor = pixel.RGB(0.2, 0.2, 0.2)
+	WindowColor = pixel.RGB(0.1, 0.1, 0.1)
 	atoms       []*Atom
 	forces      []Force
 	grid        [512][512]*Atom
@@ -42,18 +42,28 @@ type Atom struct {
 type Force struct {
 	xComponent float64
 	yComponent float64
+	Strength   float64
+	xPos       float64
+	yPos       float64
 }
 
 func handleCollision(atom1 *Atom, atom2 *Atom) {
 
 	// m1*v1 + m2*v2 = m1*v3 +m2*v4
 
-	tempVel := 0.8 * atom1.yVel
+	tempYVel := 0.8 * atom1.yVel
 	atom1.yVel = 0.8 * atom2.yVel
-	atom2.yVel = tempVel
+	atom2.yVel = tempYVel
 
 	atom1.yPos = float64(atom1.currGridYPos)
 	atom2.yPos = float64(atom2.currGridYPos)
+
+	tempXVel := 0.8 * atom1.xVel
+	atom1.xVel = 0.8 * atom2.xVel
+	atom2.xVel = tempXVel
+
+	atom1.xPos = float64(atom1.currGridXPos)
+	atom2.xPos = float64(atom2.currGridXPos)
 
 	// var atom1Pos int
 	// var sameDirection = false
@@ -82,14 +92,44 @@ func updatePostion(atom *Atom, dt float64) {
 	xForce := 0.0
 	yForce := 0.0
 	for _, force := range forces {
-		xForce += force.xComponent
-		yForce += force.yComponent
+
+		// calculate direction to force
+		// xDirection := 0.0
+		// yDirection := 0.0
+		// if force.xPos-atom.xPos != 0 {
+
+		// 	xDirection = (force.xPos - atom.xPos) / math.Abs(force.xPos-atom.xPos)
+		// }
+		// if force.yPos-atom.yPos != 0 {
+
+		// 	yDirection = (force.yPos - atom.yPos) / math.Abs(force.yPos-atom.yPos)
+		// }
+
+		// calculate angle towards force
+		angle := math.Atan2(force.xPos-atom.xPos, force.yPos-atom.yPos)
+		xComp := force.Strength * math.Sin(angle)
+		yComp := force.Strength * math.Cos(angle)
+		// fmt.Printf("%f\n", xComp)
+		// fmt.Printf("%f, %f\n", xDirection, yDirection)
+
+		xForce += xComp
+		yForce += yComp
+		// xForce += force.Strength * xDirection
+		// yForce += force.Strength * yDirection
+		// fmt.Printf("force: %f, %f\n", xForce, yForce)
 	}
-	if atom.yPos <= 0 { // touching the ground
+	if atom.yPos <= 0 || atom.yPos >= 512 { // touching the ground
 		// p = mv
 		// fmt.Print("floor\n")
 		atom.yVel = 0.8 * -atom.yVel
 		atom.yPos = float64(atom.currGridYPos)
+	}
+
+	if atom.xPos <= 0 || atom.xPos >= 512 { // touching the ground
+		// p = mv
+		// fmt.Print("floor\n")
+		atom.xVel = 0.8 * -atom.xVel
+		atom.xPos = float64(atom.currGridXPos)
 	}
 
 	xAcc := xForce / atom.mass
@@ -100,8 +140,8 @@ func updatePostion(atom *Atom, dt float64) {
 	atom.yPos = atom.yPos + (atom.yVel * dt) + (0.5 * yAcc * (dt * dt))
 	atom.yVel = atom.yVel + (yAcc * dt)
 
-	gridXPos := int(math.Max((math.Floor(atom.xPos/AtomWidth) * AtomWidth), 0))
-	gridYPos := int(math.Max((math.Floor(atom.yPos/AtomWidth) * AtomWidth), 0))
+	gridXPos := int(math.Min(math.Max((math.Floor(atom.xPos/AtomWidth)*AtomWidth), 0), 511))
+	gridYPos := int(math.Min(math.Max((math.Floor(atom.yPos/AtomWidth)*AtomWidth), 0), 511))
 
 	if grid[gridXPos][gridYPos] != nil && grid[gridXPos][gridYPos] != atom {
 		// fmt.Print("collide")
@@ -143,6 +183,19 @@ func renderAtom(atom Atom, imd *imdraw.IMDraw) {
 	imd.Rectangle(0)
 }
 
+func renderForce(force Force, imd *imdraw.IMDraw) {
+	imd.Color = pixel.RGB(1, 0, 0)
+
+	renderXPos := int(math.Floor(force.xPos/AtomWidth) * AtomWidth)
+	renderYPos := int(math.Floor(force.yPos/AtomWidth) * AtomWidth)
+
+	// imd.Push(pixel.V(float64(atom.xPos), float64(atom.yPos)))
+	// imd.Push(pixel.V(float64(atom.xPos)+AtomWidth, float64(atom.yPos)+AtomWidth))
+	imd.Push(pixel.V(float64(renderXPos), float64(renderYPos)))
+	imd.Push(pixel.V(float64(renderXPos)+AtomWidth, float64(renderYPos)+AtomWidth))
+	imd.Rectangle(0)
+}
+
 func run() {
 	cfg := pixelgl.WindowConfig{
 		Title:  "Pixel Rocks!",
@@ -158,7 +211,7 @@ func run() {
 	win.Clear(colornames.Skyblue)
 
 	// create forces
-	forces = append(forces, Force{0, Gravity})
+	forces = append(forces, Force{0, 0, Gravity, 256, -100000})
 
 	last := time.Now()
 
@@ -170,11 +223,11 @@ func run() {
 		imd.Clear() // clear the window
 
 		// add new atom to screen
-		if win.JustPressed(pixelgl.MouseButtonLeft) {
+		if win.Pressed(pixelgl.MouseButtonLeft) {
 			newColor := pixel.RGB(rand.Float64(), rand.Float64(), rand.Float64())
 			newAtom := Atom{
 				newColor,
-				10.0,
+				AtomMass,
 				win.MousePosition().X,
 				win.MousePosition().Y,
 				0,
@@ -185,10 +238,33 @@ func run() {
 			atoms = append(atoms, &newAtom)
 		}
 
+		if win.JustPressed(pixelgl.MouseButtonRight) {
+			fmt.Print("New Force\n")
+			newForce := Force{
+				0,
+				0,
+				1500,
+				win.MousePosition().X,
+				win.MousePosition().Y,
+			}
+			forces = append(forces, newForce)
+		}
+
+		if win.JustPressed(pixelgl.KeyC) {
+			fmt.Print("Cleared Force\n")
+
+			forces = []Force{}
+			forces = append(forces, Force{0, 0, Gravity, 256, -100000})
+		}
+
 		// simulate every atom
 		for _, atom := range atoms {
 			renderAtom(*atom, imd)
 			updatePostion(atom, dt)
+		}
+
+		for _, force := range forces {
+			renderForce(force, imd)
 		}
 
 		// draw to screen
